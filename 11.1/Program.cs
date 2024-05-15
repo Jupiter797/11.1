0b4a33cfbcc15aaf580171cdcdfab793745dd84d
Serhii Pypa <jupiters797@gmail.com>
2024-05-15 14:51:34 +0300

    Добавьте файлы проекта.

---
+++ 11.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._1
+{
+    delegate void MyDelegate(int number);
+    class Program
+    {
+        static void PrintNumber(int number)
+        {
+            Console.WriteLine($"Ваше число: {number}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            MyDelegate delegateInstance = new MyDelegate(PrintNumber);
+
+            Console.Write("Введіть число : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            delegateInstance(number); 
+
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._2
+{
+    internal class Program
+    {
+        delegate int MathOperation(int x, int y);
+
+        static void Calculate(int a, int b, MathOperation operation)
+        {
+            int result = operation(a, b);
+
+            Console.WriteLine($"Результат операції: {result}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Calculate(10, 3, (x, y) => x + y);
+
+            Calculate(10, 3, (x, y) => x - y);
+
+            Calculate(10, 3, (x, y) => x * y);
+
+            Calculate(10, 3, (x, y) => x / y);
+
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+    }
+}
